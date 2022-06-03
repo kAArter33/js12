535357dc580de6dc8db49bd9463556b6880443d7
kbaginski <karter1998@o2.pl>
2022-06-03 21:27:27 +0200

    poprawka

--- main.js
+++ main.js
@@ -1,32 +1,18 @@
-let tekst1 = "Uwielbiam JavaScript"
-let tekst2 = "Jestem świetnym programistą"
+let string1 = "Uwielbiam JavaScript"
+let string2 = "Jestem świetnym programistą"
 
-function porownanie(tekst1,tekst2) 
-{
+function porownanie(tekst1, tekst2) {
 
-
-if (tekst1.length > tekst2.length)
-    {
-
-    console.log("tekst1 jest dłuższy od tekst2");
-
+    if (tekst1.length > tekst2.length) {
+        return `Dłuższy jest: ${tekst1}`
     }
-else if (tekst1.length == tekst2.length)
-    
-    {
-
-
-    console.log("Długość tekstów jest taka sama");
-
+    else if (tekst1.length == tekst2.length) {
+        return `Długość tekstów jest taka sama`
     }
-
- else 
-    {
-    
-        console.log ("tekst2 jest dłuższy od tekst 1");
-   
+    else {
+        return `Dłuższy tekst: ${tekst2}`
     }
 }
 
-porownanie(tekst1,tekst2);
+console.log(porownanie(string1, string2));
 
